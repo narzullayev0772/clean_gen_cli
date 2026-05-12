3415e97abbfa83c9ce113a0ccc14044007b7ea29
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2026-05-12 16:27:04 +0500

    feat: yangi init buyruq funksionalligini qo'shildi va arxitektura papkalari tuzilishi yaratildi

--- lib/cli/command_runner.dart
+++ lib/cli/command_runner.dart
@@ -1,6 +1,5 @@
 import 'package:args/command_runner.dart';
-import 'package:clean_gen_cli/cli/commands/clean_gen_command.dart';
-import 'package:clean_gen_cli/cli/commands/create_command.dart';
+import 'package:clean_gen_cli/cli/commands/init_command.dart';
 import 'package:mason/mason.dart';
 
 class CleanGenCommandRunner extends CommandRunner<void> {
@@ -9,7 +8,6 @@ class CleanGenCommandRunner extends CommandRunner<void> {
   CleanGenCommandRunner({Logger? logger})
       : _logger = logger ?? Logger(),
         super('clean_gen', 'A CLI tool to generate clean architecture folders.') {
-    addCommand(CleanGenCommand(logger: _logger));
-    addCommand(CreateCommand(logger: _logger));
+    addCommand(InitCommand(logger: _logger));
   }
 }
