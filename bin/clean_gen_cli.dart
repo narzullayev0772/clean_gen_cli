b784514be68d590df1f6929f67efb7f02eff70ff
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2026-05-12 12:00:23 +0500

    Initialize clean_gen_cli project with basic structure and files

---
+++ bin/clean_gen_cli.dart
@@ -0,0 +1,5 @@
+import 'package:clean_gen_cli/clean_gen_cli.dart' as clean_gen_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clean_gen_cli.calculate()}!');
+}
---
+++ test/clean_gen_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:clean_gen_cli/clean_gen_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
